483d766c6dc6ee74aa6c1caf6133f2bcc5bec926
Millz98 <dave_m86@hotmail.com>
2023-05-08 17:36:29 -0600

    fix(meta): fixed website info

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,12 @@
-ocuments/astro   master +1  g cz -a                                                                              ✔  ⬢ 20.1.0  05:06:18 pm
-cz-cli@4.3.0, cz-conventional-changelog@3.3.0
-
-? Select the type of change that you're committing: chore:    Other changes that don't modify src or test files
-? What is the scope of this change (e.g. component or file name): (press enter to skip) dep
-  1 /** @type {import('tailwindcss').Config} */
-  2 module.exports = {
-  3   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  4   theme: {
-  5     extend: {},
-  6   },
-  7   plugins: [],
-  8 }
-~
-~
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        mplus: ["'M PLUS Rounded 1c'", 'Verdana', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
