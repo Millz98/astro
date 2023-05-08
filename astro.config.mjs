6eef0a76728ac53457fa27c7eb3fb650e1cb23ce
Millz98 <dave_m86@hotmail.com>
2023-05-08 17:39:14 -0600

    fix(meta): added website

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://www.youtube.com/millz98',
   integrations: [sitemap(), react(), image(), tailwind()]
 })
